52869a3f26093fc47135549d0c2b8c75efe385ab
Ronald Chanou <rchanou@gmail.com>
2015-08-01 16:03:13 -0700

    arrow keys change slides + constant element slide added to slideshow

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import Toggle from 'react-toggle';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 // with webpack and appropriate loaders, you can include non-js assets
-import './css/toggle-style.css';
+//import './css/toggle-style.css';
+//import Toggle from 'react-toggle';
 
 // slides
 import Slide1A from './slides/1a-es5-react';
-
+import slide3 from './slides/3-constant-element';
 
 const slides = [
-  <Slide1A />
+  <Slide1A />,
+  slide3
 ];
+
 
 export default class Slideshow extends React.Component {
 
@@ -19,10 +23,32 @@ export default class Slideshow extends React.Component {
   };
 
   render(){
+    console.log(this.state.slideIndex);
     return slides[this.state.slideIndex];
   }
 
   componentDidMount(){
+    $(document.body).keydown(e => {
 
+      console.log(e);
+
+      switch (e.keyCode){
+        case 37:
+          this.setState({
+            slideIndex:
+              this.state.slideIndex? (this.state.slideIndex - 1)
+              : (slides.length - 1)
+          });
+        break;
+
+        case 39:
+          this.setState({
+            slideIndex:
+              (this.state.slideIndex >= slides.length - 1)? 0
+              : (this.state.slideIndex + 1)
+          });
+        break;
+      }
+    });
   }
 }
--- src/slides/3-constant-element.js
+++ src/slides/3-constant-element.js
@@ -4,8 +4,16 @@ const header = <h1>ReactJS OC</h1>;
 
 const pageA = <div>
   {header}
+  <div>
+    This is page A.
+  </div>
 </div>;
 
 const pageB = <div>
   {header}
+  <div>
+    This is page B.
+  </div>
 </div>;
+
+export default pageB;
